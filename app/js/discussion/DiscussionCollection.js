44652fc1d0633450e865336bbcf2e44fd4575be4
sophietk <sophie.tk@gmail.com>
2015-02-14 17:11:38 +0100

    feature(message): friends can talk

--- app/js/discussion/DiscussionCollection.js
+++ app/js/discussion/DiscussionCollection.js
@@ -2,10 +2,23 @@ var DiscussionCollection = Backbone.Collection.extend({
 
     initialize: function () {
         this.listenTo(Backbone, 'addMessage', this.addMessage);
+
+        this.letThemTalk();
+        this.letThemTalk();
+        this.letThemTalk();
+        setInterval(this.letThemTalk.bind(this), 2000);
     },
 
     addMessage: function (message) {
         this.push(message);
         this.trigger('change');
+    },
+
+    letThemTalk: function () {
+        this.addMessage({
+            name: _.sample(['Jack', 'Frank', 'Vlad', 'Paul']),
+            content: chance.sentence(),
+            timestamp: new Date().getTime()
+        });
     }
 });
--- app/js/discussion/DiscussionView.js
+++ app/js/discussion/DiscussionView.js
@@ -5,16 +5,15 @@ var DiscussionView = Backbone.View.extend({
     template: $('#tpl-discussion').html(),
 
     initialize: function () {
-        this.collection = new DiscussionCollection([
-            {name: 'Jack', content: 'coucou', timestamp: 1423926206320},
-            {name: 'Jack', content: 'coucou 2', timestamp: 1423926204320},
-            {name: 'Jack', content: 'coucou 4', timestamp: 1423924206320}
-        ]);
+        this.collection = new DiscussionCollection();
         this.listenTo(this.collection, 'change', this.render);
     },
 
     render: function () {
-        var rendered = Mustache.render(this.template, {
+        var self = this,
+            rendered;
+
+        rendered = Mustache.render(this.template, {
             messages: this.collection.toJSON(),
             formatTime: function () {
                 return moment(this.timestamp).format('HH:mm:ss');
@@ -22,7 +21,6 @@ var DiscussionView = Backbone.View.extend({
         });
         this.$el.html(rendered);
 
-        var self = this;
         _.defer(function () {
             var height = self.$el[0].scrollHeight;
             self.$el.scrollTop(height);
