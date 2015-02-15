5cd6ca9c6be93567635cc93f68d23ee65279f8c8
sophietk <sophie.tk@gmail.com>
2015-02-15 11:58:31 +0100

    technical(server): dev/prod configuration

--- server/server.js
+++ server/server.js
@@ -4,23 +4,24 @@ var express = require('express'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
+    env = process.env.NODE_ENV || 'dev',
+    port = Number(process.env.PORT || 3000),
     app = express();
 
 app.use(favicon(__dirname + '/../app/img/favicon.ico'));
-app.use(logger('dev'));
+app.use(logger(env === 'prod' ? 'common' : 'dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// Serve static files
-// dev
-app.use(express.static(path.join(__dirname, '../app')));
-// dist
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
+console.log('Sever started in mode: ' + env);
+
+// Serve static files (dev or dist)
+app.use(express.static(path.join(__dirname, env === 'prod' ? '../dist' : '../app')));
 
 // Listen
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
+app.listen(port, function () {
+    console.log('Listening on port: ' + port);
 });
 
 module.exports = app;
